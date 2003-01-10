25f54d9d22f1621d63fe83e0043cbb651d918cd7
Philippe Bruhat (BooK) <book@cpan.org>
2003-01-10 12:57:38 +0000

    A different format string.

--- eg/bluecoat.pl
+++ eg/bluecoat.pl
@@ -3,12 +3,15 @@ use strict;
 use Regexp::Log::BlueCoat;
 
 my $log = Regexp::Log::BlueCoat->new(
-    format  => '%g %e %w/%s %m %%H/%d %f %A',
-    capture => [qw(c-ip)],
+    format  => '%g %e %a %w/%s %b %m %i %u %H/%d %c %f %A',
+    capture => [ ':all' ],
     ufs     => 'smartfilter',
+    login   => 'ldap',
 );
 
-my $re = $log->regexp;
-
-print ref $re, $/, $re, $/;
-
+$\ = $/;
+print $log->format;
+print $log->regexp;
+print join ' ', $log->capture;
+print '';
+print join ' ', $log->fields;
